ade8f62db17ea299f7b312f001c2a7d02569c14e
Kevin Leskinen <kevnl_95@hotmail.com>
2021-11-01 13:43:26 +0100

    added simple backend

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const express = require('express');
+
+// Create a web server - "app"
+const app = express();
+
+// Ask the web server to serve the static files in build
+app.use(express.static(path.join(__dirname, '../build')));
+
+let port = process.env.PORT || 4000;
+app.listen(port, () => console.log('Listening on port ' + port));
